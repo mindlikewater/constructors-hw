f4d9eb155335579c0637b711118b0302dfcf18a7
Tai Chen <mindlikewater@gmail.com>
2016-10-31 11:57:32 -0400

    Cleaned up functions, used ternary

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,20 +1,20 @@
 function Dog (options) {
   let opts = options || {};
 
-  this.name = opts.name;
   this.status = opts.status || "normal";
   this.color = opts.color;
 
+//shorter way to write the if-else statement
+  this.hungry = data.hungry !== undefined ? data.hungry : true;
+
+/*
   if (opts.hungry !== undefined) {
     this.hungry = opts.hungry;
   }
   else {
     this.hungry = true;
   }
-
-  if (opts.owner === "mason") {
-    this.name = "sadie";
-  }
+*/
 
 };
 
